bfc775bcdd84d01b54f4d0165f0c11bc2014ed73
Xinrong Zhao <zhaolonglong4008@gmail.com>
2018-05-02 11:52:45 -0700

    reformat

--- checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
+++ checker/src/main/java/org/checkerframework/checker/nullness/KeyForSubchecker.java
@@ -1,8 +1,5 @@
 package org.checkerframework.checker.nullness;
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.processing.SupportedOptions;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 
 /**
@@ -12,22 +9,4 @@ import org.checkerframework.common.basetype.BaseTypeChecker;
  * @checker_framework.manual #map-key-checker Map Key Checker
  * @checker_framework.manual #nullness-checker Nullness Checker
  */
-@SupportedOptions({"NullnessLite"})
-public class KeyForSubchecker extends BaseTypeChecker {
-    /**
-     * NullnessLite option for Nullness Checker 1. [Initialization Checker] disabled as command-line
-     * option. 2. [Invalidation of Dataflow] impure methods disallowed as command-line option. 3.
-     * [Invalidation of Dataflow] aliasing disallowed, see method canAlias in NullnessStore.java
-     */
-    @Override
-    public void initChecker() {
-        super.initChecker();
-
-        if (this.hasOption("NullnessLite")) {
-            Map<String, String> nullness_lite = new HashMap<String, String>();
-            nullness_lite.put("suppressWarnings", "uninitialized");
-            nullness_lite.put("assumeSideEffectFree", null);
-            this.addOptions(nullness_lite);
-        }
-    }
-}
+public class KeyForSubchecker extends BaseTypeChecker {}
